a9783c6cafcd6799a0e4680f85f245f257b355f4
akaiinoakatsuki <123093989+akaiinoakatsuki@users.noreply.github.com>
2023-02-03 00:41:21 -0300

    Add files via upload

---
+++ Proyecto final Ticmas Academy/js/cv.js
@@ -0,0 +1,6 @@
+document.getElementById("boton").onclick= function(){
+    console.log("capturamos el evento click");
+    document.getElementById("demo").innerHTML = "Bienvenidos al curriculum vitae de Diane Roberts";
+    document.getElementById("boton").style.display = "block";
+}
+
